ce449bcf20fc282d24e7148d2f64d721d6392aa9
chitrodaS <smitchitroda001@gmail.com>
2024-02-29 18:51:50 +0530

    28/2/2024

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,16 +25,33 @@ class _HomePageState extends State<HomePage> {
     startClock();
   }
 
-  bool _isAnalog = false;
-  bool _isStrap = true;
+  bool _isAnalog = true;
+  bool _isStrap = false;
+  bool _isImage = false;
+  bool _isDigital = false;
+
+  List imageUrl = [
+    "https://i.pinimg.com/564x/cc/7a/08/cc7a0805fd91dd8c5ad6457f861b7d66.jpg",
+    "https://i.pinimg.com/736x/69/6a/99/696a9941174f4fdf94b5733193e85a16.jpg",
+    "https://i.pinimg.com/236x/49/a3/83/49a3839928419e360a0f7b6f91699ef5.jpg",
+    "https://i.pinimg.com/236x/ff/67/80/ff6780d95db1821b44f64003bcb4c50b.jpg",
+    "https://i.pinimg.com/236x/74/9e/58/749e583b1f9648d693c1941670c6e82f.jpg",
+    "https://i.pinimg.com/236x/68/d0/75/68d07525cca9adab599d3c03c58ffd40.jpg",
+    "https://i.pinimg.com/236x/40/67/aa/4067aa9e6340a8f1acbaf6de5e8491b8.jpg",
+    "https://i.pinimg.com/236x/84/ab/f3/84abf3609b65fe76ff977aeaf655c126.jpg",
+    "https://i.pinimg.com/564x/87/ed/12/87ed1246e7a498f051e7d81b7670fa77.jpg",
+    "https://i.pinimg.com/564x/13/83/9d/13839d0df89247a95bf5b66efea41e6e.jpg",
+  ];
+
+  String bgImage = "";
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-            "${d.hour} : ${d.minute} : ${d.second}_Date: ${d.day}_WeekDay: ${d.weekday}"),
+        backgroundColor: Colors.black87,
+        foregroundColor: Colors.white60,
+        title: const Text("Clock page"),
       ),
       drawer: Drawer(
         child: Column(
@@ -45,6 +61,7 @@ class _HomePageState extends State<HomePage> {
                     "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png"),
                 accountName: const Text("Smit_Chitroda"),
                 accountEmail: const Text("smitchitroda@gmail.com")),
+            // AnalogWatch
             Container(
               margin: const EdgeInsets.all(5),
               child: Row(
@@ -58,6 +75,21 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            // DigitalWatch
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Digital clock"),
+                  Switch(
+                    value: _isDigital,
+                    onChanged: (val) => _isDigital = !_isDigital,
+                  ),
+                ],
+              ),
+            ),
+            // StrapWatch
             Container(
               margin: const EdgeInsets.all(5),
               child: Row(
@@ -71,10 +103,69 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Image"),
+                  Switch(
+                    value: _isImage,
+                    onChanged: (val) => _isImage = !_isImage,
+                  ),
+                ],
+              ),
+            ),
+            Visibility(
+              visible: _isImage,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: imageUrl
+                      .map(
+                        (e) => GestureDetector(
+                          onTap: () {
+                            bgImage = e;
+                            setState(() {});
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                                left: 10, right: 5, bottom: 10),
+                            height: size.height * 0.15,
+                            width: size.width * 0.25,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  offset: Offset(5, 5),
+                                  blurRadius: 7,
+                                ),
+                              ],
+                              image: DecorationImage(
+                                  image: NetworkImage(e), fit: BoxFit.fill),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
           ],
         ),
       ),
       body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: _isImage
+                  ? NetworkImage(bgImage)
+                  : const NetworkImage(
+                      "https://i.pinimg.com/1200x/84/2a/d6/842ad68b315b0f586c30b465221da609.jpg"),
+              fit: BoxFit.fill),
+        ),
         padding: const EdgeInsets.all(16),
         alignment: Alignment.center,
         child: Stack(
@@ -88,17 +179,32 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   //allPoint
                   ...List.generate(
-                      60,
-                      (index) => Transform.rotate(
-                            angle: index * (pi * 2) / 60,
-                            child: Divider(
-                              endIndent: index % 5 == 0
-                                  ? size.width * 0.88
-                                  : size.width * 0.9,
-                              thickness: 2,
-                              color: index % 5 == 0 ? Colors.red : Colors.grey,
-                            ),
-                          )),
+                    60,
+                    (index) => Transform.rotate(
+                      angle: index * (pi * 2) / 60,
+                      child: Divider(
+                        endIndent: index % 5 == 0
+                            ? size.width * 0.88
+                            : size.width * 0.9,
+                        thickness: 2,
+                        color: index % 5 == 0 ? Colors.red : Colors.grey,
+                      ),
+                    ),
+                  ),
+                  // To add number
+                  // ...List.generate(
+                  //   60,
+                  //   (index) => Transform.rotate(
+                  //     angle: index * (pi * 2) / 60,
+                  //     child: Text(
+                  //       "${index % 5 == 0 ? index : ""}",
+                  //       style: TextStyle(
+                  //         fontSize: 30,
+                  //         color: index % 5 == 0 ? Colors.red : Colors.grey,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                   //hourHand
                   Transform.rotate(
                     angle: pi / 2,
@@ -138,6 +244,35 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
+                  const CircleAvatar(
+                    radius: 8,
+                    backgroundColor: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+            Visibility(
+              visible: _isDigital,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "${d.hour} : ${d.minute} : ${d.second}",
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Day: ${d.day}",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      Text(
+                        "Week day: ${d.weekday}",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
@@ -150,7 +285,7 @@ class _HomePageState extends State<HomePage> {
                   Transform.scale(
                     scale: 7,
                     child: CircularProgressIndicator(
-                      value: d.hour.toDouble(),
+                      value: (d.hour / 12).toDouble(),
                       strokeWidth: 1.7,
                     ),
                   ),
@@ -158,7 +293,7 @@ class _HomePageState extends State<HomePage> {
                   Transform.scale(
                     scale: 5,
                     child: CircularProgressIndicator(
-                      value: d.minute.toDouble(),
+                      value: d.minute / 60,
                       strokeWidth: 1.3,
                     ),
                   ),
@@ -166,7 +301,7 @@ class _HomePageState extends State<HomePage> {
                   Transform.scale(
                     scale: 3,
                     child: CircularProgressIndicator(
-                      value: d.second.toDouble(),
+                      value: d.second / 60,
                       strokeWidth: 1.3,
                     ),
                   ),
